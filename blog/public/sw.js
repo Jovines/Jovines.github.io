951a9a9cbe200201796bea4bc2f56061e630bddd
Treeeeeeee <1246634075@qq.com>
2019-10-02 11:27:26 +0800

    update
    Signed-off-by: Treeeeeeee <1246634075@qq.com>

--- blog/public/sw.js
+++ blog/public/sw.js
@@ -4,7 +4,7 @@ workbox.setConfig({ modulePathPrefix: 'https://g.alicdn.com/kg/workbox/3.3.0/' }
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.keys().then(function (names) {
-      var validSets = ["is-sw-6878a1","is-html-6878a1","is-jsdelivr-6878a1","is-gtm-6878a1","is-gravatar-6878a1","is-theme-6878a1","is-cdn-6878a1","is-json-6878a1"];
+      var validSets = ["is-sw-a3af68","is-html-a3af68","is-jsdelivr-a3af68","is-gtm-a3af68","is-gravatar-a3af68","is-theme-a3af68","is-cdn-a3af68","is-json-a3af68"];
       return Promise.all(
         names
           .filter(function (name) { return !~validSets.indexOf(name); })
@@ -18,30 +18,30 @@ self.addEventListener('install', function (event) {
 });
 
 workbox.routing.registerRoute(new RegExp('sw\\.js'), workbox.strategies.networkOnly({
-  cacheName: 'is-sw-6878a1',
+  cacheName: 'is-sw-a3af68',
 }));
 workbox.routing.registerRoute(new RegExp('https://cdn\\.jsdelivr\\.net'), workbox.strategies.staleWhileRevalidate({
-  cacheName: 'is-jsdelivr-6878a1',
+  cacheName: 'is-jsdelivr-a3af68',
   plugins: [ new workbox.expiration.Plugin({ maxAgeSeconds: 14400 }) ],
 }));
 workbox.routing.registerRoute(new RegExp('https://www\\.googletagmanager\\.com\?id=.*'), workbox.strategies.staleWhileRevalidate({
-  cacheName: 'is-gtm-6878a1',
+  cacheName: 'is-gtm-a3af68',
   plugins: [ new workbox.expiration.Plugin({ maxAgeSeconds: 14400 }) ],
 }));
 workbox.routing.registerRoute(new RegExp('https://www\\.gravatar\\.com'), workbox.strategies.staleWhileRevalidate({
-  cacheName: 'is-gravatar-6878a1',
+  cacheName: 'is-gravatar-a3af68',
   plugins: [ new workbox.expiration.Plugin({ maxAgeSeconds: 14400 }) ],
 }));
 workbox.routing.registerRoute(new RegExp('/.*\\.(?:js|css|woff2|png|jpg|gif)$'), workbox.strategies.staleWhileRevalidate({
-  cacheName: 'is-theme-6878a1',
+  cacheName: 'is-theme-a3af68',
   plugins: [ new workbox.expiration.Plugin({ maxAgeSeconds: 14400 }) ],
 }));
 workbox.routing.registerRoute(new RegExp('https://cdn\\.yourdomain\\.com'), workbox.strategies.staleWhileRevalidate({
-  cacheName: 'is-cdn-6878a1',
+  cacheName: 'is-cdn-a3af68',
   plugins: [ new workbox.expiration.Plugin({ maxAgeSeconds: 14400 }) ],
 }));
 workbox.routing.registerRoute(new RegExp('your_data_prefix/.*\\.json'), workbox.strategies.cacheFirst({
-  cacheName: 'is-json-6878a1',
+  cacheName: 'is-json-a3af68',
   plugins: [ new workbox.expiration.Plugin({ maxAgeSeconds: 14400 }) ],
 }));
 
---
+++ blog/push.bat
@@ -0,0 +1,4 @@
+git add .
+git commit -s -m "update"
+git pull --rebase
+git push
